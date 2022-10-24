118083b7b277e4bebbc81461c2b5d2c036a22a9f
joaocorner <joaoeduardocorner@gmail.com>
2022-10-23 22:32:40 -0300

    added the basic info and buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
 
 function App() {
-  return <div>Genius Game</div>;
+  return (
+    <div>
+      <h1>Genius Game</h1>
+      <h2>Score: 0</h2>
+      <h2>Round: 1</h2>
+      <h2>Time: 0</h2>
+      <h2>High Score: 0</h2>
+      <h2>High Round: 1</h2>
+      <h2>High Time: 0</h2>
+
+      <button>Start</button>
+      <button>Stop</button>
+      <button>Reset</button>
+
+      <div>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
