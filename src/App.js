1420ac21a38a874197f5d5ba6f06521f552148d1
joaocorner <joaoeduardocorner@gmail.com>
2022-10-27 11:35:17 -0300

    loop to generate random numbers and put them in an array
    deleting uneccessary states and translating the page

--- src/App.js
+++ src/App.js
@@ -3,41 +3,23 @@ import "./App.css";
 import Button from "./components/Button";
 
 function App() {
-  const [generatedColors, setGeneratedColors] = useState([]);
-  const [round, setRound] = useState(2);
-
-  const generateColor = () => {
-    // generate random number between 1 and 4
-    const randNum = Math.floor(Math.random() * 4) + 1;
-    // return a color based on the random number
-    switch (randNum) {
-      case 1:
-        setGeneratedColors([...generatedColors, "red"]);
-        break;
-      case 2:
-        setGeneratedColors([...generatedColors, "green"]);
-        break;
-      case 3:
-        setGeneratedColors([...generatedColors, "blue"]);
-        break;
-      case 4:
-        setGeneratedColors([...generatedColors, "yellow"]);
-        break;
-      default:
-        console.log("error");
-    }
-  };
+  const [start, setStart] = useState(false);
+  const [generatedNumber, setGeneratedNumber] = useState(0);
+  const [red, setRed] = useState(false);
+  const [green, setGreen] = useState(false);
+  const [blue, setBlue] = useState(false);
+  const [yellow, setYellow] = useState(false);
 
   const initGame = () => {
-    for (let i = 0; i < round; i++) {
-      setTimeout(() => {
-        generateColor();
-      }, 1000 * i);
-    }
-  };
+    // reseting all states
+    setStart(true);
+    setGeneratedNumber([]);
 
-  const increaseRound = () => {
-    setRound(round + 1);
+    // loop to generate random numbers and put them in an array
+    for (let i = 0; i < 15; i++) {
+      const randomColor = Math.floor(Math.random() * 4) + 1;
+      setGeneratedNumber((prev) => [...prev, randomColor]);
+    }
   };
 
   return (
@@ -45,29 +27,43 @@ function App() {
       <header>
         <h1>Genius Game</h1>
         <div>
-          <h2>High Score: 0</h2>
-          <h2>High Round: 1</h2>
-          <h2>High Time: 0</h2>
+          <h2>Melhor rodada: 1</h2>
         </div>
       </header>
       <main>
         <div className="action-buttons">
-          <button onClick={initGame}>Start</button>
-          <button>Stop</button>
-          <button>Reset</button>
+          <button onClick={initGame} disabled={start}>
+            Iniciar
+          </button>
+          <button>Parar</button>
+          <button>Reiniciar</button>
         </div>
 
         <div className="actual-info">
-          <h2>Score: 0</h2>
-          <h2>Round: 1</h2>
-          <h2>Time: 0</h2>
+          <h2>Rodada: 1</h2>
         </div>
 
-        <div className="table-colors">
-          <Button text="1" color="red" />
-          <Button text="2" color="blue" />
-          <Button text="3" color="green" />
-          <Button text="4" color="yellow" />
+        <div className="table-colors ">
+          <Button
+            text="1"
+            color="red"
+            className={`btn-colors ${red ? "active" : ""}`}
+          />
+          <Button
+            text="2"
+            color="blue"
+            className={`btn-colors ${blue ? "active" : ""}`}
+          />
+          <Button
+            text="3"
+            color="green"
+            className={`btn-colors ${green ? "active" : ""}`}
+          />
+          <Button
+            text="4"
+            color="yellow"
+            className={`btn-colors ${yellow ? "active" : ""}`}
+          />
         </div>
       </main>
     </div>
